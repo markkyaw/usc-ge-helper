6886021b2429155193f1f72d1cc9011554854523
Paing Kyaw <annonymous10101@gmail.com>
2020-03-11 01:36:58 -0700

    finished setting up for nodejs

--- main.js
+++ main.js
@@ -1,80 +1,41 @@
-// $.ajax(
-// {
-//     method: "GET",
-//     url: "http://cors-anywhere.herokuapp.com/https://classes.usc.edu/term-20201/classes/arts/",
-// }
-// ).done(function(response)
-// {
-//     console.log(response);
-//     var data = {html: html};
-//     var json = JSON.stringify(data);
-// }).fail(function()
-// {
-//     console.log("failed");
-// });
-
-// // var name = "codemzy";var url = "http://anyorigin.com/go?url=" + encodeURIComponent("https://www.codewars.com/users/") + name + "&callback=?";$.get(url, function(response) {  console.log(response);});
-
-
-// Custom Select
-// Source: https://codepen.io/yy/pen/vOYqYV
-// https://freefrontend.com/css-select-boxes/
-$(".custom-select").each(function() 
+document.querySelector("#r3").onsubmit = function(event)
 {
-    var classes = $(this).attr("class"), id = $(this).attr("id"), name = $(this).attr("name");
-    var template = '<div class="' + classes + '">';
-    template += '<span class="custom-select-trigger">' + $(this).attr("placeholder") + "</span>";
-    template += '<div class="custom-options">';
-    $(this)
-        .find("option")
-        .each(function() 
-        {
-            template += '<span class="custom-option ' + $(this).attr("class") + '" data-value="' + $(this).attr("value") + '">' + $(this).html() + "</span>";
-        }
-        );
-    template += "</div></div>";
-    $(this).wrap('<div class="custom-select-wrapper"></div>');
-    $(this).hide();
-    $(this).after(template);
-});
+    event.preventDefault();
 
-$(".custom-option:first-of-type").hover(
-function() {
-    $(this)
-    .parents(".custom-options")
-    .addClass("option-hover");
-},
-function() {
-    $(this)
-    .parents(".custom-options")
-    .removeClass("option-hover");
+    // get the year
+    let y = document.getElementById("year");
+    let yr = y.options[y.selectedIndex].value;
+    // get the term
+    let t = document.getElementById("term");
+    let term = t.options[t.selectedIndex].value;
+    let termText = t.options[t.selectedIndex].text;
+    // get the first GE category
+    let c1 = document.getElementById("c1");
+    let GE1 = c1.options[c1.selectedIndex].value;
+    // get the second GE category
+    let c2 = document.getElementById("c2");
+    let GE2 = c2.options[c2.selectedIndex].value;
+
+    // Remove prev search results
+    let tableHead = document.querySelector("thead");
+    tableHead.removeChild(tableHead.lastChild);
+    let tableBody = document.querySelector("tbody");
+    while (tableBody.hasChildNodes())
+        tableBody.removeChild(tableBody.lastChild);
+    
+    // Create <th> in thead
+    let trTag = document.createElement("tr");
+    let thTag = document.createElement("th");
+    thTag.innerHTML = "GEs that are in category " + GE1 + " and " + GE2 + " in " + termText + " for " + yr + ": ";
+    trTag.appendChild(thTag);
+    document.querySelector("thead").appendChild(trTag);
+
+    // do some scripting stuff
+
+    trTag = document.createElement("tr");
+    let tdTag = document.createElement("td");
+    tdTag.innerHTML = "";
+    trTag.appendChild(tdTag);
+    document.querySelector("tbody").appendChild(trTag);
+
 }
-);
-$(".custom-select-trigger").on("click", function() {
-$("html").one("click", function() {
-    $(".custom-select").removeClass("opened");
-});
-$(this)
-    .parents(".custom-select")
-    .toggleClass("opened");
-event.stopPropagation();
-});
-$(".custom-option").on("click", function() {
-$(this)
-    .parents(".custom-select-wrapper")
-    .find("select")
-    .val($(this).data("value"));
-$(this)
-    .parents(".custom-options")
-    .find(".custom-option")
-    .removeClass("selection");
-$(this).addClass("selection");
-$(this)
-    .parents(".custom-select")
-    .removeClass("opened");
-$(this)
-    .parents(".custom-select")
-    .find(".custom-select-trigger")
-    .text($(this).text());
-});
-  
