4256732b7c8ca0ac737ae344661505e67d616997
Paing Kyaw <annonymous10101@gmail.com>
2020-04-05 17:18:10 -0700

    minor changes

--- app/config/connection.js
+++ app/config/connection.js
@@ -7,10 +7,10 @@ var mysql = require("mysql");
 
 // Set up our connection information
 var connection = mysql.createConnection({
-  host: "mydeliverymyanmar.com",
-  port: "3306",
-  user: "mydertxq_markk418",
-  password: "Lllh91L0x4qZ",
+  host: 'markkyaw.com',
+  port: '3306',
+  user: 'mydertxq_markk418',
+  password: 'Lllh91L0x4qZ',
 });
 
 // Connect to the database
@@ -19,11 +19,11 @@ connection.connect(function(err)
 	if(err) throw err;
 	console.log("Connected");
 
-	connection.query(sql, function(err, result)
-	{
-		if(err) throw err;
-		console.log("Result: " + result);
-	});
+	// connection.query(sql, function(err, result)
+	// {
+	// 	if(err) throw err;
+	// 	console.log("Result: ' + result);
+	// });
 });
 
 // Export connection
